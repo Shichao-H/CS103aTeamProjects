48c6fff4be8a39d9dce6a019f48ddee711a675c5
Shichao-H <123280612+Shichao-H@users.noreply.github.com>
2023-03-18 20:26:26 -0400

    Add files via upload

--- ca01/gptwebapp.py
+++ ca01/gptwebapp.py
@@ -1,22 +1,5 @@
 '''
-gptwebapp shows how to create a web app which ask the user for a prompt
-and then sends it to openai's GPT API to get a response. You can use this
-as your own GPT interface and not have to go through openai's web pages.
 
-We assume that the APIKEY has been put into the shell environment.
-Run this server as follows:
-
-On Mac
-% pip3 install openai
-% pip3 install flask
-% export APIKEY="......."  # in bash
-% python3 gptwebapp.py
-
-On Windows:
-% pip install openai
-% pip install flask
-% $env:APIKEY="....." # in powershell
-% python gptwebapp.py
 '''
 from flask import request,redirect,url_for,Flask
 from gpt import GPT
@@ -26,20 +9,41 @@ app = Flask(__name__)
 gptAPI = GPT(os.environ.get('APIKEY'))
 
 # Set the secret key to some random bytes. Keep this really secret!
-app.secret_key = b'_5#y2L"F4Q789789uioujkkljkl...8z\n\xec]/'
+# app.secret_key = b'_5#y2L"F4Q789789uioujkkljkl...8z\n\xec]/'
 
 @app.route('/')
-def index():
+def home():
     ''' display a link to the general query page '''
     print('processing / route')
     return f'''
-        <h1>GPT Demo</h1>
-        <a href="{url_for('gptdemo')}">Ask questions to GPT</a>
+        <h1>CA 01</h1>
+        <a href="/about">About page</a>
+        <a href="/team">Team page</a>
+        <a href="/index">Index page</a>
+    '''
+
+@app.route('/about')
+def about():
+    return "<h1>About Page</h1>This is a program that can do lots of useful things"
+
+@app.route('/team')
+def team():
+    return f'''
+        <h1>Team Page</h1>
+        Shichao He: Sophomore, CS major, author of factorization function
+    '''
+
+@app.route('/index')
+def index():
+    return f'''
+        <h1>Index Page</h1>
+        <a href="/index/factorization">Factorizing a polynomial</a>
     '''
 
 
-@app.route('/gptdemo', methods=['GET', 'POST'])
-def gptdemo():
+#The factorization funciton will get a polynomial and give the result after the factorization
+@app.route('/index/factorization', methods=['GET', 'POST'])
+def factorization():
     ''' handle a get request by sending a form 
         and a post request by returning the GPT response
     '''
@@ -50,16 +54,14 @@ def gptdemo():
         <h1>GPT Demo</h1>
         <pre style="bgcolor:yellow">{prompt}</pre>
         <hr>
-        Here is the answer in text mode:
+        Here is the result of polynomial factorization:
         <div style="border:thin solid black">{answer}</div>
-        Here is the answer in "pre" mode:
-        <pre style="border:thin solid black">{answer}</pre>
-        <a href={url_for('gptdemo')}> make another query</a>
+        <a href=/factorizing> do another factorization</a>
         '''
     else:
         return '''
-        <h1>GPT Demo App</h1>
-        Enter your query below
+        <h1>Shichao He factorizing app</h1>
+        Enter your polynomial that needs to be factorized(start with factorizing)
         <form method="post">
             <textarea name="prompt"></textarea>
             <p><input type=submit value="get response">
