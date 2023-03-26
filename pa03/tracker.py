491f0a3b67f3520cbad2823fc7ca7ebd3d5c3785
Shichao-H <123280612+Shichao-H@users.noreply.github.com>
2023-03-26 16:50:17 -0400

    Add files via upload

--- pa03/tracker.py
+++ pa03/tracker.py
@@ -15,7 +15,29 @@ def print_usage():
         '''
         )
 
-def print(transaction):
+def print_trans(trans):
 
 def process_args(arglist):
-    
+    transaction = Transaction()
+    if arglist==[]:
+        print_usage()
+    elif arglist[0]=="show":
+        print_trans(transaction.showTrans())
+    elif arglist[0]=="add":
+        if len(arglist)!=3:
+            print_usage()
+        else:
+            trans = {'amount':arglist[1],'category':arglist[2],'date':arglist[3],'description':arglist[4]}
+            transaction.addTrans(trans)
+    elif arglist[0]=="delete":
+        if len(arglist)!= 2:
+            print_usage()
+        else:
+            print_trans(transaction.deleteTrans(arglist[1]))
+    elif arglist[0]=='summarize':
+        if arglist[2]== 'date':
+        
+        elif arglist[2]=='month':
+        
+        elif arglist[2]=='year':
+            
