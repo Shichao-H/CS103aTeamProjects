49a5c7a34cde4c3897bfd7063cd21f8e0e8a4825
xiaoyangz1 <123614256+xiaoyangz1@users.noreply.github.com>
2023-03-26 19:12:10 -0400

    correct mistakes

--- pa03/transaction.py
+++ pa03/transaction.py
@@ -3,22 +3,23 @@ import os
 
 def toDict(t):
     print('t=' + str(t))
-    transaction = {'item #':t[0], 'amount':t[1], 'category':t[2], 'date':t[3], 'description':t[4]}
-    return transaction
+    transactions = {'item #':t[0], 'amount':t[1], 'category':t[2], 'date':t[3], 'description':t[4]}
+    return transactions
 
 def Transaction():
     def __init__(self):
-        self.runQuery('''CREATE TABLE IF NOT EXISTS transaction 
-                    (item # text, amount int, category text, date text, description text)''',())
-
+        self.runQuery('''CREATE TABLE IF NOT EXISTS transactions 
+                    (rowid INTEGER PRIMARY KEY AUTOINCREMENT, amount int, category text, date text, description text)''',())
+        
     def showTrans(self):
-        return self.runQuery("SELECT * FROM transaction",())
+        return self.runQuery("SELECT rowid, * FROM transactions",())
     
     def addTrans(self, item):
-        return self.runQuery("INSERT INTO transaction (?,?,?,?)",(item['amount'], item['category'], item['date]'], item['description']))
+        return self.runQuery("INSERT INTO transactions (amount, category, date, description) VALUES (?, ?, ?, ?)",
+                             (item['amount'], item['category'], item['date'], item['description']))
                              
     def deleteTrans(self,itemid):
-        return self.runQuery("DELETE FROM transaction WHERE item # = (?)",(itemid,))
+        return self.runQuery("DELETE FROM transactions WHERE rowid=(?)",(itemid,))
     
     def sumTransbyDate(self):
         return self.runQuery("SELECT date, SUM(amount) FROM transaction GROUP BY date",())
@@ -53,7 +54,7 @@ def Transaction():
     def runQuery(self, query, tuple):
         con = sqlite3.connect(os.getenv('HOME') + '/transaction.db')
         cur = con.cursor()
-        cur.excute(query, tuple)
+        cur.execute(query, tuple)
         tuples = cur.fetchall()
         con.commit()
         con.close()
