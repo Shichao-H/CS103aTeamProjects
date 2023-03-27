0f05836d561fb928e3ac38b0f86ab47cee205a57
XuCai021130 <115910501+XuCai021130@users.noreply.github.com>
2023-03-26 20:22:49 -0400

    Update transaction.py

--- pa03/transaction.py
+++ pa03/transaction.py
@@ -6,55 +6,58 @@ def toDict(t):
     transactions = {'item #':t[0], 'amount':t[1], 'category':t[2], 'date':t[3], 'description':t[4]}
     return transactions
 
-def Transaction():
+class Transaction():
     def __init__(self):
-        self.runQuery('''CREATE TABLE IF NOT EXISTS transactions 
+        self.run_query('''CREATE TABLE IF NOT EXISTS transactions 
                     (rowid INTEGER PRIMARY KEY AUTOINCREMENT, amount int, category text, date text, description text)''',())
-        
-    def showTrans(self):
-        return self.runQuery("SELECT rowid, * FROM transactions",())
+
+    def quit(self):
+        return "hide transactions"
+
+    def show_transaction(self):
+        return self.run_query("SELECT * FROM transactions",())
     
-    def addTrans(self, item):
-        return self.runQuery("INSERT INTO transactions (amount, category, date, description) VALUES (?, ?, ?, ?)",
+    def add_transaction(self, item):
+        return self.run_query("INSERT INTO transactions (amount, category, date, description) VALUES (?, ?, ?, ?)",
                              (item['amount'], item['category'], item['date'], item['description']))
                              
-    def deleteTrans(self,itemid):
-        return self.runQuery("DELETE FROM transactions WHERE rowid=(?)",(itemid,))
+    def delete_transaction(self,itemid):
+        return self.run_query("DELETE FROM transactions WHERE rowid=(?)",(itemid,))
     
-    def sumTransbyDate(self):
-        return self.runQuery("SELECT date, SUM(amount) FROM transaction GROUP BY date",())
+    def sum_trans_by_date(self):
+        return self.run_query("SELECT date, SUM(amount) FROM transaction GROUP BY date",())
     
-    def sumTransbyMonth(self):
+    def sum_Trans_by_month(self):
         '''
         Summarize all the transactions by sum of the months
         Author: Charles Cai
         '''
 
-        return self.runQuery('''
+        return self.run_query('''
         SELECT strftime("%Y-%M", date) AS month, SUM(amount) AS sum 
-        FROM transaction 
+        FROM transactions 
         GROUP BY month
         ORDER BY month DESC''',())
 
-    def sumTransbyYear(self):
+    def sum_trans_by_year(self):
         '''
         Summarize all the transactions by sum of the years
         Author: Charles Cai
         '''
 
-        return self.runQuery('''
+        return self.run_query('''
         SELECT strftime("%Y", date) AS year, SUM(amount) AS sum 
-        FROM transaction 
+        FROM transactions 
         GROUP BY year
         ORDER BY year DESC''',())
 
-    def sumTransbyCategory(self):
-        return self.runQuery("SELECT category, SUM(amount) FROM transaction GROUP BY category",())
+    def sum_trans_by_category(self):
+        return self.run_query("SELECT category, SUM(amount) FROM transactions GROUP BY category",())
     
-    def runQuery(self, query, tuple):
-        con = sqlite3.connect(os.getenv('HOME') + '/transaction.db')
+    def run_query(self, query, tuple):
+        con = sqlite3.connect('transactions.db')
         cur = con.cursor()
-        cur.execute(query, tuple)
+        cur.execute(query,tuple)
         tuples = cur.fetchall()
         con.commit()
         con.close()
